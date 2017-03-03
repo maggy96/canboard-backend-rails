349f781e08374f47de37edd90f10094d82b776e9
Magnus Görlitz <magnus.goerlitz@makandra.de>
2017-03-04 00:54:24 +0100

    refactor user spec and add board spec

--- spec/controllers/api/v1/boards_controller_spec.rb
+++ spec/controllers/api/v1/boards_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-describe Api::V1::UsersController do
+describe Api::V1::BoardsController do
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
+      @board = FactoryGirl.create :board
       auth_headers = @user.create_new_auth_token
       request.headers.merge!(auth_headers)
       get :show, params: { id: @user.id }
@@ -13,10 +14,11 @@ describe Api::V1::UsersController do
       expect(response.code).to eq('200')
     end
 
-    it 'returns the serialized user attributes' do
-      expect(JSON.parse(response.body)['data']['attributes']).to eq(
-        {"name" => "Thomas A. Anderson", 
-         "email" => "thomas.a.anderson@metacortex.com" })
+    it 'returns the serialized board attributes' do
+      expect(JSON.parse(response.body)['data'][0]['attributes']).to eq(
+        {"title" => "Project: Exit Matrix", 
+         "description" => "Follow the white rabbit",
+         "cards" => [] })
     end
   end
 end
