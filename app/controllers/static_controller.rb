ebf551970e8549dcf4c43ad00401785b5de3e8d8
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-03-12 00:12:19 +0100

    Rewrite static controller in a way that should be quick and work for all environments (both development and production) using regexes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,51 +1,33 @@
 class StaticController < ApplicationController
 
   def show
-    if Rails.env.development?
-      show_development
-    else
-      show_production
-    end
+    file = params[:file]
+    render_static find_file(file, default_file)
   end
 
   private
 
-  # A lot of different files (especially js) are loaded and need to be served.
-  # Use a file-search based solution here
-  def show_development
-    file = params[:file]
-    if not (file.nil? || file.blank?)
-      [dynamic_path, resource_path].each do |path|
-        f = File.join(path, file)
-        if File.exist?(f)
-          render_static f
-          return
-        end
-      end
-    end
-    render_static default_file
-  end
-
-  # Only certain allowed files should be served
-  # Make a hash of allowed files and look up the path
-  def show_production
-    file = params[:file]
-    render_static file_map.fetch(file, default_file)
-  end
-
   def file_map
-    js_path = dynamic_path.join('js')
-    css_path = resource_path.join('css')
-    img_path = resource_path.join('img')
-    { 'js/app.min.js'               => js_path.join('app.min.js'),
-      'js/app.js'                   => js_path.join('app.min.js'),
-      'js/main.js'                  => js_path.join('app.min.js'),
-      'img/logo/logo-large.png'     => img_path.join('logo', 'logo-large.png'),
-      'img/logo/logo-navbar.png'    => img_path.join('logo', 'logo-navbar.png'),
-      'css/site.css'                => css_path.join('site.min.css'),
-      'css/site.min.css'            => css_path.join('site.min.css'),
-      'css/vendor/semantic.min.css' => css_path.join('vendor', 'semantic.min.css')
+    {
+      /js\/(.*)/    => dynamic_path,
+      /css\/(.*)/   => resource_path,
+      /img\/(.*)/   => resource_path,
+      /(.*)\.html/  => resource_path
     }
+  end
+
+  def find_file(path, default)
+    # Find all regexes from file_map that match the given path
+    matches = file_map.keys.inject([]) {|arr, p|
+      m = p.match(path)
+      m.nil? ? arr : arr.push(p)
+    }
+    # If there are no matches, return the default
+    return default if matches.nil? || matches.empty?
+    # Else, concat the path to the base-path given by file_map
+    file = file_map[matches.first].join(path)
+    # Return the file at the resulting path, if it exists
+    file.exist? ? file : default
   end
 
   def render_static(file)
