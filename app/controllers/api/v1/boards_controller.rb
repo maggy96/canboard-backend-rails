5675632709eb28e8fbce09ddc81c58ba80d2eda9
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-03-16 22:53:50 +0100

    Add 'create' endpoint for boards

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -10,6 +10,17 @@ module Api
       def show
         render json: @current_v1_user.boards.where(id: params[:id])
       end
+
+      # POST /v1/boards
+      def create
+        board = Board.new(params.require(:board).permit(:title, :description))
+        board.user = @current_v1_user
+        if board.save
+          render json: board
+        else
+          render json: { error: "Could not create board" }, status: 400
+        end
+      end
     end
   end
 end
