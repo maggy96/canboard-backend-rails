60cdca24b8ab62613ce61f59a1fc837dfcac03d7
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-03-27 01:31:57 +0200

    Fix user boards field in lists_controller

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -3,12 +3,12 @@ module Api
     class ListsController < ApiController
       # GET /v1/board/{id}/lists
       def index
-        render json: @current_v1_user.board.find(params[:board_id]).lists
+        render json: @current_v1_user.boards.find(params[:board_id]).lists
       end
 
       # GET /v1/board/{id}/lists/{id}
       def show
-        render json: @current_v1_user.board.find(params[:board_id]).lists.find(params[:list_id])
+        render json: @current_v1_user.boards.find(params[:board_id]).lists.find(params[:list_id])
       end
 
       def create
