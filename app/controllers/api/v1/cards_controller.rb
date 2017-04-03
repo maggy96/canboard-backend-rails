c66df0ce25250d9aea2b4a5fcbf34518d4cd90af
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-04-03 23:05:32 +0200

    Fix typo in cards-controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -3,12 +3,12 @@ module Api
     class CardsController < ApiController
       # GET /v1/boards/{id}/list/{id}/cards
       def index
-        render json: @current_v1_user.boards.find(params[:board_id]).list.find(params[:list_id]).cards
+        render json: @current_v1_user.boards.find(params[:board_id]).lists.find(params[:list_id]).cards
       end
 
       # GET /v1/boards/{id}/list/{id}/cards/{id}
       def show
-        render json: @current_v1_user.boards.find(params[:board_id]).list.find(params[:list_id]).cards.find(params[:card_id])
+        render json: @current_v1_user.boards.find(params[:board_id]).lists.find(params[:list_id]).cards.find(params[:card_id])
       end
     end
   end
